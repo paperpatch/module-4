9e8349ef9bc0dc7880b577b107a882d31340ec24
paperpatch <patchen21@gmail.com>
2021-10-08 15:17:15 -0400

    save tasks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,8 +66,8 @@ var createTaskEl = function(taskDataObj) {
   
   // increase task counter for next unique id
   taskIdCounter++;
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
+
+  saveTasks();
 };
 
 var createTaskActions = function(taskId) {
@@ -131,6 +131,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
   formEl.removeAttribute("data-task-id");
   // update formEl button to go back to saying "Add Task" instead of "Edit Task"
   formEl.querySelector("#save-task").textContent = "Add Task";
+
+  saveTasks();
 };
 
 var taskButtonHandler = function(event) {
@@ -175,6 +177,8 @@ var taskStatusChangeHandler = function(event) {
       tasks[i].status = statusValue;
     }
   }
+
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -219,7 +223,13 @@ var deleteTask = function(taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks();
 };
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
